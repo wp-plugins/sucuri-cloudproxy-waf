5ceb10e6a29b870818c2a10458858893be880ac3
yorman <yorman@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-06-10 02:05:32 +0000

    Fixed. Avoid directory listing
    git-svn-id: https://plugins.svn.wordpress.org/sucuri-cloudproxy-waf/trunk@929295 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Avoid directory listing.
+ *
+ * @package   Sucuri Plugin - CloudProxy WAF
+ * @author    Daniel Cid   <dcid@sucuri.net>
+ * @copyright Since 2013-2014 Sucuri Inc.
+ * @license   Released under the GPL - see LICENSE file for details.
+ * @link      https://cloudproxy.sucuri.net/
+ * @since     File available since Release 0.1
+ */
+
+if( !defined('SUCURIWAF') ){ exit(0); }
